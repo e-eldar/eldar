1c6c4b54ce60f780ad194b9c27c71a6071b4bc7e
e-eldar <jusic905eldar@gmail.com>
2025-01-17 13:49:05 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,20 +2,21 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+//Send message when new client is connected. Notification always pops out
 wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('message', (message) => {
     console.log('Received:', message);
 
-    // Broadcast to all clients
+    // Broad cast to all clients 
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     });
   });
-
+//On close, connection stop. Client loses connection. 
   ws.on('close', () => {
     console.log('Client disconnected');
   });
